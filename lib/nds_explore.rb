9c5cf82b5bfa728f955941690f0547812e6d4617
premti <premti@gmail.com>
2020-02-26 13:34:18 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,9 +9,9 @@ end
 
 def print_first_directors_movie_titles
   element_index = 0 
-  while element_index < directors_database[0] do 
+  while element_index < directors_database.length do 
     hash_index = 0 
-    while hash_index < directors_database < directors_database[0][:name]
+    while hash_index < directors_database < directors_database[0]
     pp directors_database[:name][:movies][:title]
     hash_index += 1
   end
