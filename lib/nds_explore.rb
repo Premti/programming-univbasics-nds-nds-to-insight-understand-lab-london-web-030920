1a8b65522459203dc339ceea61447e6631ba0a29
premti <premti@gmail.com>
2020-02-26 13:52:05 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 def print_first_directors_movie_titles
   element_index = 0 
   while element_index < directors_database do 
-    pp directors_database[element_index][:movies][element_index][:title]
+    pp directors_database[0][:movies][element_index][:title]
    element_index += 1
   end 
 end
